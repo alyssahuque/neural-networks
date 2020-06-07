8c342ad98f2574df304f5cd905cda5bb92175956
alyssahuque <alyssahuque@gmail.com>
2020-06-07 14:15:43 -0700

    training

--- five.py
+++ five.py
@@ -7,10 +7,23 @@
 
 # look at the tutorials for training data set
 
+# review of literature
+# thoughts of deep learning
+# plot some graphs, prediction accuracy
+# different results for each game seperately
+# show results
+# what if you change number of nodes and layers for hidden layers
+# train in different game, train single neural net
+# wrap up
+
+
 # no convolutions
 
+import torch
 from torch import nn
 from collections import OrderedDict
+import torch.nn.functional as F
+from torchvision import datasets, transforms
 
 class Network(nn.Module):
     def __init__(self):
@@ -60,6 +73,37 @@ def main():
         ('softmax', nn.Softmax(dim=1))]))
     print(model[0])
     print(model.fc1)
+
+    # Define a transform to normalize the data
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,)),])
+
+    # Download and load the training data\
+    trainset = datasets.MNIST('~/Users/alyssahuque/Downloads/Data/8Target-3Resource/Group1/Game_1', download=True, train=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+
+    model = nn.Sequential(nn.Linear(784, 128),nn.ReLU(),nn.Linear(128, 64),nn.ReLU(),nn.Linear(64, 10),nn.LogSoftmax(dim=1))
+    # Define the loss
+    criterion = nn.NLLLoss()
+    # Optimizers require the parameters to optimize and a learning rate
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.003)
+    epochs = 5
+    for e in range(epochs):
+    	running_loss = 0
+    	for data, labels in trainloader:
+    		# Flatten MNIST data into a 784 long vector
+    		data = data.view(data.shape[0], -1)
+
+    		# Training pass
+    		optimizer.zero_grad()
+
+    		output = model(data)
+    		loss = criterion(output, labels)
+    		loss.backward()
+    		optimizer.step()
+
+    		running_loss += loss.item()
+    	else:
+    		print(f"Training loss: {running_loss/len(trainloader)}")
 
 if __name__ == "__main__":
     main()
